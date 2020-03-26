57c41db9184205f779c2b8c0c149f6dba66b641f
Jonatan Treviño Galindo <galtrev@gmail.com>
2020-03-26 16:48:26 -0700

    Init setup

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testMatch: ['**/*.(test|spec).(ts|tsx)'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  globals: {
+    // XXX we must specify a custom tsconfig for tests because we need the typescript transform
+    //  to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+    //  can see this setting in tsconfig.jest.json -> "jsx": "react"
+    //  See https://github.com/zeit/next.js/issues/8663
+    'ts-jest': {
+      tsConfig: 'jest.tsconfig.json',
+    },
+  },
+  setupFiles: ['./enzyme.config.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@elements(.*)$': '<rootDir>/src/elements$1',
+  },
+};
