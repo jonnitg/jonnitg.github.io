c30869f260a4ae537cd146d1b1a23b0307567212
Jonatan Treviño Galindo <galtrev@gmail.com>
2020-03-26 17:17:26 -0700

    Change code to develop branch

---
+++ src/components/layout/layout.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return <div> huere</div>;
+};
+
+export default Layout;
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import Head from 'next/head';
+import Layout from '@components/layout';
+
+const Home = () => <Layout>here</Layout>;
+
+export default Home;
