10054d2a1f7471cf9514530b8d30c2d0871c2b63
Jonatan Treviño Galindo <galtrev@gmail.com>
2020-03-27 19:31:46 -0700

    Add Color Picker and theme configuration

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PointerClassesProvider } from '@context/pointer-classes.context';
 import useMousePointer from '@hooks/useMousePointer';
 import MouseTracker from '@elements/mouse-tracker';
+import ThemeSettings from '@components/theme-settings';
 import styles from './layout.module.scss';
 
 const Layout = ({ children }) => {
@@ -9,6 +10,7 @@ const Layout = ({ children }) => {
 
   return (
     <PointerClassesProvider>
+      <ThemeSettings />
       <main ref={setRef} className={styles.stl}>
         {children}
         <MouseTracker mousePosition={mousePosition} />
---
+++ src/components/theme-settings/theme-settings.component.tsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import { TwitterPicker } from 'react-color';
+import styles from './theme-settings.module.scss';
+
+const colors = [
+  '#f44336',
+  '#e91e63',
+  '#9c27b0',
+  '#673ab7',
+  '#3f51b5',
+  '#2196f3',
+  '#03a9f4',
+  '#00bcd4',
+  '#009688',
+  '#4caf50',
+  '#8bc34a',
+  '#cddc39',
+  '#ffeb3b',
+  '#ffc107',
+  '#ff9800',
+  '#ff5722',
+  '#795548',
+  '#607d8b',
+];
+
+const ThemeSettings = () => {
+  const [showThemePicker, setShowThemePicker] = useState(false);
+  const [showTextPicker, setShowTextPicker] = useState(false);
+
+  const handleChangeComplete = (color, type) => {
+    if (type === 'theme') {
+      document.documentElement.style.setProperty(
+        '--main-theme-color',
+        color.hex
+      );
+      window.localStorage.setItem('themeColor', color.hex);
+    }
+    if (type === 'text') {
+      document.documentElement.style.setProperty(
+        '--main-text-color',
+        color.hex
+      );
+      window.localStorage.setItem('textColor', color.hex);
+    }
+  };
+
+  const toggleOptions = (type) => {
+    if (type === 'theme') {
+      setShowThemePicker(!showThemePicker);
+      setShowTextPicker(false);
+    }
+    if (type === 'text') {
+      setShowThemePicker(false);
+      setShowTextPicker(!showTextPicker);
+    }
+  };
+
+  useEffect(() => {
+    const { localStorage } = window;
+    const currentColor = localStorage.getItem('themeColor');
+
+    if (currentColor) {
+      const found = colors.find(
+        (color) => color === currentColor.trim(),
+        currentColor
+      );
+
+      document.documentElement.style.setProperty('--main-theme-color', found);
+    }
+  }, []);
+
+  return (
+    <div className={styles['theme-settings']}>
+      <div>
+        <button
+          type="button"
+          className={styles['theme-settings__button']}
+          onClick={() => toggleOptions('theme')}
+        >
+          Theme Color
+        </button>
+        {showThemePicker && (
+          <TwitterPicker
+            className={styles['theme-settings__picker']}
+            colors={colors}
+            width={315}
+            onChangeComplete={(color) => handleChangeComplete(color, 'theme')}
+          />
+        )}
+      </div>
+
+      <div>
+        <button
+          type="button"
+          className={styles['theme-settings__button']}
+          onClick={() => toggleOptions('text')}
+        >
+          Text Color
+        </button>
+
+        {showTextPicker && (
+          <TwitterPicker
+            className={styles['theme-settings__picker']}
+            colors={colors}
+            width={315}
+            onChangeComplete={(color) => handleChangeComplete(color, 'text')}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ThemeSettings;
