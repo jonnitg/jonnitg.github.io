90591fac6debcb2fee839f51511b4d6b8bdb1802
Jonatan Treviño Galindo <galtrev@gmail.com>
2020-03-29 17:30:29 -0700

    Add text for unsupported landscape mobile view

--- src/components/about-section/about-section.component.tsx
+++ src/components/about-section/about-section.component.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import clsx from 'clsx';
 import Heading from '@elements/heading';
 import styles from './about-section.module.scss';
 
 const AboutSection = () => {
+  const aboutTextClasses = clsx(
+    styles.stl__container,
+    styles['stl__about-text']
+  );
+
+  const hobbiesTextClasses = clsx(
+    styles.stl__container,
+    styles['stl__hobbies-text']
+  );
+
   return (
     <div className={styles.stl}>
-      <div>
-        <Heading>About me</Heading>
+      <div className={aboutTextClasses}>
+        <Heading tag="h2">About me</Heading>
       </div>
-      <div>
-        <Heading>Hobbies</Heading>
+      <div className={hobbiesTextClasses}>
+        <Heading tag="h2">Hobbies</Heading>
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,21 @@ import { AppProps } from 'next/app';
 import '../sass/main.scss';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className="hidden-on-landscape">
+        <div className="hidden-on-landscape__container">
+          <h1 className="hidden-on-landscape__heading">
+            It&apos;s not broken, just unsupported
+          </h1>
+          <p className="hidden-on-landscape__helper-text">
+            Please, flip the device
+          </p>
+        </div>
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
