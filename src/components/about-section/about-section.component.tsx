eab8f9694c1892e58fe297eeabfcb0203705b0be
Jonatan Treviño Galindo <galtrev@gmail.com>
2020-05-20 00:44:16 -0700

    Add useSectionObserver

--- src/components/about-section/about-section.component.tsx
+++ src/components/about-section/about-section.component.tsx
@@ -1,47 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import clsx from 'clsx';
+import useSectionObserver from '@hooks/useSectionObserver';
 import Heading from '@elements/heading';
 import styles from './about-section.module.scss';
 
 const AboutSection = () => {
+  const aboutRef = useRef(null);
+  useSectionObserver(aboutRef, { root: null, threshold: [0.5] });
   const aboutTextClasses = clsx(
     styles.stl__container,
     styles['stl__about-section']
   );
 
-  const hobbiesTextClasses = clsx(
-    styles.stl__container,
-    styles['stl__hobbies-section']
-  );
-
   return (
-    <div className={styles.stl}>
-      <div className={aboutTextClasses}>
-        <Heading tag="h2">About me</Heading>
-        <p className={styles.stl__text}>
-          I&apos;m a person passionate about the development of web
-          technologies, and who uses technologies to create a new experience and
-          who is able to demonstrate everything he knows how to do.
-        </p>
-        <p className={styles.stl__text}>
-          Well organized person, self-taught, problem solver, and with high
-          attention to detail. A family person, the one that makes them laugh.
-        </p>
-        <p className={styles.stl__text}>
-          Interested in the frontend environment and working on ambitious
-          projects with a good community.
-        </p>
+    <section ref={aboutRef} id="about" className="section">
+      <div className={styles.stl}>
+        <div className={aboutTextClasses}>
+          <Heading tag="h2">About me</Heading>
+          <p className={styles.stl__text}>
+            I&apos;m a person passionate about the development of web
+            technologies, and who uses technologies to create a new experience
+            and who is able to demonstrate everything he knows how to do.
+          </p>
+          <p className={styles.stl__text}>
+            Well organized person, self-taught, problem solver, and with high
+            attention to detail. A family person, the one that makes them laugh.
+          </p>
+          <p className={styles.stl__text}>
+            Interested in the frontend environment and working on ambitious
+            projects with a good community.
+          </p>
+        </div>
       </div>
-      <div className={hobbiesTextClasses}>
-        <Heading tag="h2">Hobbies</Heading>
-        <ul className={styles.stl__list}>
-          <li>Musician</li>
-          <li>PC Gamer</li>
-          <li>Watch Anime</li>
-          <li>Watch TV series</li>
-        </ul>
-      </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/skills-section/skills-section.component.tsx
+++ src/components/skills-section/skills-section.component.tsx
@@ -1,56 +1,62 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import useSectionObserver from '@hooks/useSectionObserver';
 import Heading from '@elements/heading';
 import styles from './skills-section.module.scss';
 
 const SkillsSection = () => {
+  const skillsRef = useRef(null);
+  useSectionObserver(skillsRef, { root: null, threshold: [0.5] });
+
   return (
-    <div className={styles.stl}>
-      <div className={styles.stl__container}>
-        <Heading tag="h2" className={styles.str__heading}>
-          Skills
-        </Heading>
-        <div className={styles.stl__text}>
-          <ul>
-            <li>
-              Front-End
-              <ul>
-                <li>HTML</li>
-                <li>CSS / SASS</li>
-                <li>Javascript ES6 / JQuery</li>
-                <li>Typescript</li>
-              </ul>
-            </li>
-            <li>
-              Back-End
-              <ul>
-                <li>Javascript / NodeJs</li>
-                <li>PHP / Composer</li>
-                <li>Docker</li>
-              </ul>
-            </li>
-            <li>
-              FrameWorks
-              <ul>
-                <li>React / Redux / GatsbyJs / NextJs</li>
-                <li>Material UI</li>
-                <li>GraphQL</li>
-                <li>Laravel</li>
-                <li>AdonisJs / ExpressJs</li>
-                <li>Firebase</li>
-              </ul>
-            </li>
-            <li>
-              Source Control
-              <ul>
-                <li>Git (Git Flow)</li>
-                <li>Github</li>
-                <li>Jira</li>
-              </ul>
-            </li>
-          </ul>
+    <section ref={skillsRef} id="skills" className="section">
+      <div className={styles.stl}>
+        <div className={styles.stl__container}>
+          <Heading tag="h2" className={styles.str__heading}>
+            Skills
+          </Heading>
+          <div className={styles.stl__text}>
+            <ul>
+              <li>
+                Front-End
+                <ul>
+                  <li>HTML</li>
+                  <li>CSS / SASS</li>
+                  <li>Javascript ES6 / JQuery</li>
+                  <li>Typescript</li>
+                </ul>
+              </li>
+              <li>
+                Back-End
+                <ul>
+                  <li>Javascript / NodeJs</li>
+                  <li>PHP / Composer</li>
+                  <li>Docker</li>
+                </ul>
+              </li>
+              <li>
+                FrameWorks
+                <ul>
+                  <li>React / Redux / GatsbyJs / NextJs</li>
+                  <li>Material UI</li>
+                  <li>GraphQL</li>
+                  <li>Laravel</li>
+                  <li>AdonisJs / ExpressJs</li>
+                  <li>Firebase</li>
+                </ul>
+              </li>
+              <li>
+                Source Control
+                <ul>
+                  <li>Git (Git Flow)</li>
+                  <li>Github</li>
+                  <li>Jira</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
