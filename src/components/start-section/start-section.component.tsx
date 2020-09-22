4d5f0d66dd0aca0b03fa671ff90fba511f47b875
Jonatan Treviño Galindo <galtrev@gmail.com>
2020-09-21 22:20:14 -0700

    redesign!: add styles to start section
    TODO: mobile responsive

--- src/components/start-section/start-section.component.tsx
+++ src/components/start-section/start-section.component.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import Heading from '@elements/heading';
 import Breakline from '@elements/breakline';
 import CodeText from '@elements/code-text';
+import Heading from '@elements/heading';
 import styles from './start-section.module.scss';
 
 const StartSection: React.FunctionComponent = () => {
   return (
-    <section id="start">
-      <div>
-        <img src="" alt="" />
-        <CodeText>user.image</CodeText>
-      </div>
-      <div>
-        <CodeText>user.full_name</CodeText>
-        <Heading tag="h1">
+    <section id="start" className="section">
+      <div className={styles.text__container}>
+        <CodeText className={styles.text__heading__code}>
+          user.full_name
+        </CodeText>
+        <Heading tag="h1" className={styles.text__heading}>
           Jonatán Isaac
           <Breakline />
           Treviño Galindo
         </Heading>
-        <Heading tag="h2">Web Developer</Heading>
-        <CodeText>user.title</CodeText>
+        <Heading tag="h2" className={styles.text__subheading}>
+          Web Developer
+        </Heading>
+        <CodeText className={styles.text__subheading__code}>
+          user.title
+        </CodeText>
+      </div>
+      <div className={styles.image__container}>
+        <img
+          className={styles.image}
+          src="https://via.placeholder.com/420x800"
+          alt=""
+        />
+        <CodeText className={styles.image__text}>user.image</CodeText>
       </div>
     </section>
   );
